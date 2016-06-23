bf014dacc61e2a3766ac607261e1d8f1180e49e3
garry <girishdagdiya28@gmail.com>
2016-06-23 09:33:11 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplicationConstReadOnly
+{
+    class Program
+    {
+
+        readonly int j = 3;
+        internal const int i = 1;
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine("i=" + i + 2);
+            Console.WriteLine("j=" + p.j);
+            Console.ReadLine();
+
+        }
+        public Program()
+        {
+
+            j = 2;
+            
+        }
+    }
+}
